c40c58b57c180f76901713d79ca25613c458fd7f
Yuvraj Singh <yuvi1707@hotmail.com>
2023-06-10 22:09:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 window.onload = function() {
+    
+    // Variables to store elements
     var display = document.getElementById('display');
     var startBtn = document.getElementById('startBtn');
     var stopBtn = document.getElementById('stopBtn');
     var resetBtn = document.getElementById('resetBtn');
     var timeList = document.getElementById('timeList');
-
+    
+    // Variables to perform calculations
     var startTime;
     var elapsedTime = 0;
     var timerInterval;
     var running = false;
     
+    // Funtion to format time in HH:MM:SS:MM 
     function formatTime(milliseconds) {
       var hours = Math.floor(milliseconds / 3600000);
       var minutes = Math.floor((milliseconds % 3600000) / 60000);
@@ -23,25 +27,29 @@ window.onload = function() {
       return formattedTime;
     }
     
+    // Function which upadtes the display time 
     function updateDisplay() {
       var currentTime = new Date().getTime();
       var deltaTime = currentTime - startTime;
       display.textContent = formatTime(elapsedTime + deltaTime);
     }
     
+    // Function to control the start button 
     function startTimer() {
+      // if the timer is not running it will turn its running state to true 
       if (!running) {
         startTime = new Date().getTime();
         timerInterval = setInterval(updateDisplay, 10);
         running = true;
       }
     }
-    
+    // function to control the stop button
     function stopTimer() {
+      // checks if timer is running or not 
        if (running) {
         clearInterval(timerInterval);
         elapsedTime += new Date().getTime() - startTime;
-        running = false;
+        running = false; 
   
         // Store and display the stopped time
         var stopTime = formatTime(elapsedTime);
@@ -51,6 +59,8 @@ window.onload = function() {
     }
     }
     
+
+    // Reset the timer all to zero 
     function resetTimer() {
       clearInterval(timerInterval);
       display.textContent = '00:00:00:000';
@@ -62,6 +72,7 @@ window.onload = function() {
 
     }
     
+    // Calling the above functions when the user clicks any button 
     startBtn.addEventListener('click', startTimer);
     stopBtn.addEventListener('click', stopTimer);
     resetBtn.addEventListener('click', resetTimer);
